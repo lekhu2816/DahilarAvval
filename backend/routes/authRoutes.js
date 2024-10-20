0d569f602079cadb374a308bbf8ca0103123fec7
Lekhansh Sachan <sachanlekhansh@gmail.com>
2024-10-20 06:42:19 +0530

    Added forget and reset password routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { signup,login,logout } from "../controller/userAuthController.js";
+import { signup,login,logout ,verifyUser, resetPassword, forgetPassword} from "../controller/userAuthController.js";
 const authRoute=express.Router();
 
 authRoute.post('/signup',signup);
+authRoute.post('/verify',verifyUser);
 authRoute.post('/login',login);
 authRoute.post('/logout',logout);
+authRoute.post('/forget-password',forgetPassword);
+authRoute.post('/reset-password',resetPassword);
 
 export default authRoute;
