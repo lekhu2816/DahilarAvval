c944b90d8de4c8a5cf864efcdbc5c45b7f617d0f
Lekhansh Sachan <sachanlekhansh@gmail.com>
2024-10-23 06:37:48 +0530

    added spinner component

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
+import Loader from "../../component/loader/loader";
 const Login = () => {
   const navigate=useNavigate();
+  const[loading,setLoading]=useState(false);
+  const onSubmitForm=(e)=>{
+    e.preventDefault()
+    setLoading(true)
+  }
+  
   return (
     <div className="login-page">
       <div className="login">
@@ -11,7 +18,7 @@ const Login = () => {
         </div>
         <div className="section-right">
             <h1 className="login-title">Welcome</h1>
-          <form>
+          <form onSubmit={onSubmitForm}>
             <div className="input-field">
             <i className="fa-regular fa-envelope"></i>
               <input type="text" placeholder="Enter you email" required />
@@ -24,7 +31,7 @@ const Login = () => {
             <div className="forget-password">
               <p>forget password?</p>
             </div>
-            <button>Login</button>
+            <button>{!loading?<>Create Account</>:<div className="loading-btn">Please Wait<Loader/></div>}</button>
           </form>
           <div className="create-account">
             <p>Don't have account ? <span onClick={()=>navigate('/signup')}>Create account</span></p>
--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -1,14 +1,84 @@
-import React from 'react'
-import './signup.css'
+import React, { useState } from "react";
+import "./signup.css";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../component/loader/loader";
 const Signup = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+  };
   return (
-    <div className='signup-page'>
-        <div className="signup">
-          <div className="section-left"></div>
-          <div className="section-right"></div>
+    <div className="signup-page">
+      <div className="signup">
+        <div className="section-left">
+          <h1>Welcome to Dahilar Awwal</h1>
         </div>
-    </div>
-  )
-}
+        <div className="section-right">
+          <h1 className="signup-title">Create an account</h1>
+          <form onSubmit={onFormSubmit}>
+            <div className="input-field">
+              <i className="fa-regular fa-user"></i>
+              <input type="text" placeholder="Username" required />
+            </div>
+            <div className="input-field">
+              <i class="fa-regular fa-envelope"></i>
+              <input type="text" placeholder="Enter your email" required />
+            </div>
+            <div className="input-field">
+              <i className="fa-solid fa-lock"></i>
+              <input
+                type={showPassword ? "password" : "text"}
+                placeholder="Password"
+                required
+              />
+              <i
+                onClick={() => setShowPassword((prev) => !prev)}
+                className={`fa-regular ${
+                  showPassword ? "fa-eye" : "fa-eye-slash"
+                }`}
+              ></i>
+            </div>
+            <div className="input-checkbox">
+              <input type="checkbox" name="" id="" required />
+              <p>By continuing, i agree the terms of use & privacy policy</p>
+            </div>
+            <button>
+              {!loading ? (
+                <>Create Account</>
+              ) : (
+                <div className="loading-btn">
+                  Please Wait
+                  <Loader />
+                </div>
+              )}
+            </button>
+          </form>
+          <div className="have-account">
+            <p>
+              Already have account ?
+              <span
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                {" "}
+                Login
+              </span>
+            </p>
+          </div>
 
-export default Signup
+          <div className="login-admin">
+            <p>
+              Login as admin ? <span>Login</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
